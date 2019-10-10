c28a8619e9cde9b191fd13e90849d2dcff2f9647
TecnologiaWeb19 <53907286+TecnologiaWeb19@users.noreply.github.com>
2019-10-10 15:11:56 -0400

    Clase JavaScript
    Se realizo la clase de javascript en fecha 10/10/2019, la fecha 04-10-19, Por motivos de aniversario de la carrera.

---
+++ 6_Clase_JS_04-10-19/javascript/miscript.js
@@ -0,0 +1,9 @@
+function saludo(){
+    alert('Hola Mundo');
+    document.getElementById("parrafo").innerHTML="Se Cambio a Hola Mundo";
+}
+
+function CambiarFondo(){
+    document.body.style.background='blue'; 
+    document.getElementById('titulo').style.color='white';
+}
